c459e36105d9bc6c4412ea1bd206fc940bceb3b5
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2020-11-25 18:11:31 -0500

    Refactored Footer to components and rename to index. Renamed icon images, renamed LoginIndex to index2

--- App.js
+++ App.js
@@ -1,15 +1,58 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import LoginIndex from './src/screens/Login/LoginIndex';
-import HomeIndex from './src/screens/Home/HomeIndex';
-import FooterIndex from './src/screens/Footer/FooterIndex';
-import UserInputIndex from './src/screens/UserInput/UserInputIndex';
-import LogoutIndex from './src/screens/Logout/LogoutIndex';
-import UserExitIndex from './src/screens/UserExit/UserExitIndex'
+import React, { useState, useEffect, useCallback, useRef } from "react";
+import { View, ActivityIndicator } from 'react-native'
+import { Provider } from "react-redux";
+import store from './src/config/store'
+import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/components/AuthContext";
+import { auth } from "./src/config/firebase";
+import RootStack from "./src/navigators/RootStack"
 
+const App = () => {
+  const [currentUser, setUser] = useState("");
+  const [initialRouteName, setInitialRouteName] = useState("Login");
+  const [loginState, setLoginState] = useState(false);
 
-const navigator = createStackNavigator({
-  Screen: UserExitIndex
-});
+  const updateUserState = useCallback((user) => {
+    // console.log("[App/updateUserState] ", user);
+    if (user) {
+      // console.log("[metadata] ", auth.currentUser.metadata);
+      // console.log(user)
+      setUser(user);
+      setInitialRouteName("Home");
+    } else {
+      setUser(null);
+      setInitialRouteName("Login");
+    }
+    setLoginState(false);
+  }, []);
 
-export default createAppContainer(navigator);
+  useEffect(() => {
+    setLoginState(true);
+    // listen for auth state changes
+    const unsubscribe = auth.onAuthStateChanged(updateUserState);
+    // unsubscribe to the listener when unmounting
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loginState || initialRouteName === "") {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator size={50} color={"#2DD47F"} />
+      </View>
+    );
+  }
+
+  return (
+    <Provider store={store}>
+      <AuthProvider value={{ currentUser }}>
+        <NavigationContainer>
+          <RootStack initialRouteName={initialRouteName} />
+        </NavigationContainer>
+      </AuthProvider>
+    </Provider>
+  );
+
+}
+
+export default App;
