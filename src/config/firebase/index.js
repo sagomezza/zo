c8450e1dcdf26fbac21c51c0de101f1fbfc14926
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2020-11-25 13:44:45 -0500

    Firebase config and API constants

---
+++ src/config/firebase/index.js
@@ -0,0 +1,14 @@
+import { firebase } from '@firebase/app';
+import '@firebase/auth';
+import '@firebase/database';
+import "@firebase/firestore"
+import service_account from "./service_account.json";
+
+firebase.initializeApp(service_account);
+
+const auth = firebase.auth();
+// const database = firebase.database();
+const firestore = firebase.firestore();
+
+export { auth, firestore };
+export default firebase;
