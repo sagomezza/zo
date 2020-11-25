a8c72ad784ec1f111daefff8d1d0be7896d70008
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2020-11-25 18:06:46 -0500

    Module navigators

---
+++ src/navigators/RootStack/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Login from "../../screens/Login";
+
+import HomeStack from "../HomeStack";
+// import HeaderTitle from "components/StackHeader";
+
+const Stack = createStackNavigator();
+
+const RootStack = ({ initialRouteName }) => {
+  return (
+    <Stack.Navigator
+      initialRouteName={initialRouteName}
+      // headerMode='none'
+      screenOptions={{
+        //headerTitle: (props) => <HeaderTitle {...props} />,
+        headerStyle: {
+          backgroundColor: "#2DD47F",
+          height: 100,
+        },
+        // headerTitleAlign: "center",
+        headerTitleAlign: "left",
+        headerTitleStyle: {
+          color: "white",
+        },
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        // headerLeftContainerStyle: { borderWidth: 2, borderColor: "red" },
+        headerTitleContainerStyle: { left: "15%" },
+        // headerLeft: () => (
+        //     <MaterialIcons.Button
+        //         name={"arrow-back"}
+        //         backgroundColor="lightgray"
+        //         size={40}
+        //         onPress={() => {
+        //             navigation.goBack();
+        //         }}
+        //     />
+        // ),
+      }}
+    >
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{
+          headerShown: false,
+          //title: "Mi perfil",
+        }}
+      />
+      
+      <Stack.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          headerShown: false,
+          // title: "Home",
+        }}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default RootStack;
