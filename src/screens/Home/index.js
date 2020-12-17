0329810bf584ab1f1acf1562442e81079ef060dd
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2020-12-17 16:15:41 -0500

    Getting token from redux and updating official profile

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, Image  } from 'react-native';
+import { View, Text, FlatList, Image } from 'react-native';
 import FooterIndex from '../../components/Footer';
 import HomeStyles from '../Home/HomeStyles';
 import Button from '../../components/Button';
 import instance from "../../config/axios";
-import { GET_RECIPS, READ_HQ } from "../../config/api";
+import { GET_RECIPS, READ_HQ, EDIT_OFFICIAL } from "../../config/api";
 import { connect } from "react-redux";
 import * as actions from "../../redux/actions";
 import store from '../../config/store';
@@ -20,9 +20,9 @@ const HomeIndex = (props) => {
         const response = await instance.post(GET_RECIPS, {
           hqId: officialProps.hq[0]
         });
-        console.log(response.data)
-        if(response.data.response === 1){
-          
+        // console.log(response.data)
+        if (response.data.response === 1) {
+
           store.dispatch(actions.setRecips(response.data.data.finished));
         }
       } catch (error) {
@@ -30,12 +30,23 @@ const HomeIndex = (props) => {
       }
     };
 
+    const updateExpoToken = async () => {
+      try {
+        await instance.post(EDIT_OFFICIAL, {
+          id: officialProps.id,
+          expoToken: props.expoToken.expoToken
+        });
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
     const readHq = async () => {
       try {
         const response = await instance.post(READ_HQ, {
           id: officialHq
         });
-        if(response.data.response){
+        if (response.data.response) {
           store.dispatch(actions.setReservations(response.data.data.reservations));
           store.dispatch(actions.setHq(response.data.data));
         }
@@ -46,106 +57,110 @@ const HomeIndex = (props) => {
 
     getRecips();
     readHq();
+    updateExpoToken();
   }, []);
 
   return (
-    <View style={{flex: 1,backgroundColor:"#FFFFFF"}}>
+    <View style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
       <View>
-      <View style={{heigth: '14%'}} >
-      <Button onPress={()=> navigation.navigate("Logout")} 
-              title="Cerrar sesión" 
-              color="transparent" 
-              style={{ borderWidth:1, 
-                       borderColor: "#008999", 
-                       alignSelf: 'flex-end',
-                       width: '30%',
-                       heigth: '10%',
-                       marginRight:'5%',
-                       marginTop:'6%'
-                      }} 
-              textStyle={{color: "#008999"}} />
-      </View>
-      <View style={{alignItems:"flex-end", width: '100%', height: '20%'}}>
-      <View style={HomeStyles.plateContainer}>
-        <View style={HomeStyles.plateInput}>
-        <Image style={{width:"24%", height: "24%", marginTop: "5%", marginLeft: "5%"}} resizeMode={"contain"} source={require( '../../../assets/images/TrazadoM.png' )}/>
-          <Text style={HomeStyles.plateInputText}>
-            {`${hq.availableBikes}/${hq.totalBikes}`}
-          </Text>
-          
+        <View style={{ heigth: '14%' }} >
+          <Button onPress={() => navigation.navigate("Logout")}
+            title="Cerrar sesión"
+            color="transparent"
+            style={{
+              borderWidth: 1,
+              borderColor: "#008999",
+              alignSelf: 'flex-end',
+              width: '30%',
+              heigth: '10%',
+              marginRight: '5%',
+              marginTop: '6%'
+            }}
+            textStyle={{ color: "#008999" }} />
         </View>
-        <View style= {HomeStyles.plateInput}>
-        <Image style={{width:"25%", height: "25%", marginTop: "5%", marginLeft: "5%"}} resizeMode={"contain"} source={require( '../../../assets/images/TrazadoC.png' )}/>
-          <Text style={HomeStyles.plateInputText}>
-          {`${hq.availableCars}/${hq.totalCars}`}
-          </Text>
-        </View>
-      </View>
-      </View>
-      <View style={{ height: '72%', paddingBottom: '10%'}}>
-        <View style={{marginLeft: '13%',marginBottom:'2%'}}>
-          <Text style={HomeStyles.textPago} >Historial de pagos</Text>
-        </View>
-        <View style={{height: "49%"}}>
-        {recips.recips.length > 0 ? 
-        <FlatList
-          style= {{height: "37%"}}
-          data={recips.recips}
-          keyExtractor={({ id }) => id}
-          renderItem={({item}) => {
-          return (
-          <View style={{ flexDirection: "row", position: 'relative',  borderBottomWidth: 1, borderColor: "#008999", marginBottom: '4%', marginLeft: '13%', marginRight:'13%', marginTop: '2%' }} >
-            <View style={{marginBottom: '2%'}} >
-              <Text   style={HomeStyles.textPlaca}>{item.plate}</Text>
-              <Text   style={HomeStyles.textPago}>{`Pago por ${item.hours} horas`}</Text>
+        <View style={{ alignItems: "flex-end", width: '100%', height: '20%' }}>
+          <View style={HomeStyles.plateContainer}>
+            <View style={HomeStyles.plateInput}>
+              <Image style={{ width: "24%", height: "24%", marginTop: "5%", marginLeft: "5%" }} resizeMode={"contain"} source={require('../../../assets/images/TrazadoM.png')} />
+              <Text style={HomeStyles.plateInputText}>
+                {`${hq.availableBikes}/${hq.totalBikes}`}
+              </Text>
+
             </View>
-            <View style={{ flex: 1, alignItems:'flex-end'}} >
-              <Text  style={HomeStyles.textMoney}>${item.total}</Text>
+            <View style={HomeStyles.plateInput}>
+              <Image style={{ width: "25%", height: "25%", marginTop: "5%", marginLeft: "5%" }} resizeMode={"contain"} source={require('../../../assets/images/TrazadoC.png')} />
+              <Text style={HomeStyles.plateInputText}>
+                {`${hq.availableCars}/${hq.totalCars}`}
+              </Text>
             </View>
           </View>
-          )}}
-        />
-        :
-        <View style={{paddingLeft:60, marginTop: 10}}>
-          <Text style={HomeStyles.textPago}> No se encuentran registros en el historial </Text>
         </View>
-        }   
-        </View>
-        <View style= {{height:"36%", marginTop:'4%'}}>
-        <View style={{marginLeft: '13%', marginBottom:'2%'}}>
-          <Text style={HomeStyles.textPago}>Carros parqueados</Text>
-        </View>
-         
-        {reservations.reservations.length > 0 ? 
-          <FlatList
-          styles={{marginTop: '2%'}}
-          data={reservations.reservations}
-          keyExtractor={({ id }) => id}
-          renderItem={({item}) => {
-          return (
-            <View style={{ flex:1, flexDirection: "row", position: 'relative',  borderBottomWidth: 1, borderColor: "#FFFFFF", marginLeft: '13%', marginRight:'13%', marginTop: '4%' }} >            
-            <View style={{marginBottom: 10, marginleft:10}} >
-              <Text  style={HomeStyles.textPlaca} >{item.plate}</Text>
-              <Text style={HomeStyles.textPago}>{item.verificationCode}</Text>
-            </View>
-            <View style={{ flex: 1, alignItems:'flex-end'}} >
-          <Text style={HomeStyles.textMoney}>{moment(item.dateStart).format('LT')}</Text>
-              <Text style={HomeStyles.textPago}>Pago por horas</Text>
-            </View>
+        <View style={{ height: '72%', paddingBottom: '10%' }}>
+          <View style={{ marginLeft: '13%', marginBottom: '2%' }}>
+            <Text style={HomeStyles.textPago} >Historial de pagos</Text>
           </View>
-          )}}    
-        /> :
-        <View style={{paddingLeft:60, marginTop: 10}}>
-          <Text style={HomeStyles.textPago}>No hay parqueos activos en este momento</Text>
+          <View style={{ height: "49%" }}>
+            {recips.recips.length > 0 ?
+              <FlatList
+                style={{ height: "37%" }}
+                data={recips.recips}
+                keyExtractor={({ id }) => id}
+                renderItem={({ item }) => {
+                  return (
+                    <View style={{ flexDirection: "row", position: 'relative', borderBottomWidth: 1, borderColor: "#008999", marginBottom: '4%', marginLeft: '13%', marginRight: '13%', marginTop: '2%' }} >
+                      <View style={{ marginBottom: '2%' }} >
+                        <Text style={HomeStyles.textPlaca}>{item.plate}</Text>
+                        <Text style={HomeStyles.textPago}>{`Pago por ${item.hours} horas`}</Text>
+                      </View>
+                      <View style={{ flex: 1, alignItems: 'flex-end' }} >
+                        <Text style={HomeStyles.textMoney}>${item.total}</Text>
+                      </View>
+                    </View>
+                  )
+                }}
+              />
+              :
+              <View style={{ paddingLeft: 60, marginTop: 10 }}>
+                <Text style={HomeStyles.textPago}> No se encuentran registros en el historial </Text>
+              </View>
+            }
+          </View>
+          <View style={{ height: "36%", marginTop: '4%' }}>
+            <View style={{ marginLeft: '13%', marginBottom: '2%' }}>
+              <Text style={HomeStyles.textPago}>Carros parqueados</Text>
+            </View>
+
+            {reservations.reservations.length > 0 ?
+              <FlatList
+                styles={{ marginTop: '2%' }}
+                data={reservations.reservations}
+                keyExtractor={({ id }) => id}
+                renderItem={({ item }) => {
+                  return (
+                    <View style={{ flex: 1, flexDirection: "row", position: 'relative', borderBottomWidth: 1, borderColor: "#FFFFFF", marginLeft: '13%', marginRight: '13%', marginTop: '4%' }} >
+                      <View style={{ marginBottom: 10, marginleft: 10 }} >
+                        <Text style={HomeStyles.textPlaca} >{item.plate}</Text>
+                        <Text style={HomeStyles.textPago}>{item.verificationCode}</Text>
+                      </View>
+                      <View style={{ flex: 1, alignItems: 'flex-end' }} >
+                        <Text style={HomeStyles.textMoney}>{moment(item.dateStart).format('LT')}</Text>
+                        <Text style={HomeStyles.textPago}>Pago por horas</Text>
+                      </View>
+                    </View>
+                  )
+                }}
+              /> :
+              <View style={{ paddingLeft: 60, marginTop: 10 }}>
+                <Text style={HomeStyles.textPago}>No hay parqueos activos en este momento</Text>
+              </View>
+            }
+          </View>
+
+
         </View>
-        }
-        </View>
-        
-        
-      </View>
       </View>
       <FooterIndex navigation={navigation} />
-      
+
     </View>
   );
 };
@@ -155,7 +170,8 @@ const mapStateToProps = (state) => ({
   officialProps: state.official,
   reservations: state.reservations,
   recips: state.recips,
-  hq: state.hq
+  hq: state.hq,
+  expoToken: state.expoToken
 });
 
 export default connect(mapStateToProps, actions)(HomeIndex);
