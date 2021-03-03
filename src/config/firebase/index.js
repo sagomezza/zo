73486ca7b149798cb6e7d968f8ba3e8d6787031e
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2021-03-03 09:38:37 -0500

    Updated gitignore

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -2,7 +2,7 @@ import { firebase } from '@firebase/app';
 import '@firebase/auth';
 import '@firebase/database';
 import "@firebase/firestore"
-import service_account from "./service_account.json";
+import service_account from "./service_account_dev.json";
 
 firebase.initializeApp(service_account);
 
