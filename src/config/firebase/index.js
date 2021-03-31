e8cc6433d5be15b9afea0c0d92b761454eb3627a
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo-2.local>
2021-03-31 17:12:16 -0500

    serviceaccount prod

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -2,7 +2,7 @@ import { firebase } from '@firebase/app';
 import '@firebase/auth';
 import '@firebase/database';
 import "@firebase/firestore"
-import service_account from "./service_account.json";
+import service_account from "./service_account_prod.json";
 
 // const database = firebase.database();
 
