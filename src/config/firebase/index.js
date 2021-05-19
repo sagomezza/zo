cb800e3ad678ff4134dd6201a1f2b3865a806f03
Santiago Gomez Zabala <sgomez@novacompcr.com>
2021-05-19 09:20:54 -0500

    Fixed crashing issue with importin firebase storage

--- src/config/firebase/index.js
+++ src/config/firebase/index.js
@@ -2,6 +2,7 @@ import { firebase } from '@firebase/app';
 import '@firebase/auth';
 import '@firebase/database';
 import "@firebase/firestore"
+import '@firebase/storage'
 import service_account from "./service_account_prod.json";
 
 // const database = firebase.database();
