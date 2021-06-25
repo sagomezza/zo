d656356c41c009789ae662096bffca8ca1451ffa
AlejandraYepes <maria@bluspot.us>
2021-06-25 12:58:16 -0500

    Changed organization name for sentry

--- App.js
+++ App.js
@@ -102,7 +102,7 @@ const App = () => {
   Sentry.init({
     dsn: 'https://022b0475f7b147aba62d6d1988bf95df@o479500.ingest.sentry.io/5644578',
     enableInExpoDevelopment: true,
-    debug: false, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+    debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
   });
 
   const readUser = async (userEmail) => {
